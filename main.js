8b0a1b81fc9196aaeb0d02a269ff7de9c070788d
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-09-23 15:21:40 +0530

    removed animation cz of a bug

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ setInterval(() => {
   
     const diff = endDate - now;
 
+    if(endDate - now < 0){
+      alert("The date has already passed !");
+    }
+
     // these are the values of how many of milliseconds make one of them like 10000 ms = 1s and so on cz timestamp is in milliseconds
     const seconds = 1000;
     const minutes = seconds * 60;
@@ -15,38 +19,42 @@ setInterval(() => {
     const days = hours * 24;
   
     let timeDays = Math.floor(diff / days);
+    timeDays = timeDays < 10 ? "0" + timeDays : timeDays;
 
-    let finalDays = 0;
-    setInterval(() => {
-      document.getElementById("days").innerHTML = finalDays;
+    // let finalDays = 0;
+    // setInterval(() => {
+    //   document.getElementById("days").innerHTML = finalDays;
 
-      if(finalDays === timeDays){
-        clearInterval();
-      }else{
-        finalDays++;
-      }
-    }, 100);
+    //   if(finalDays === timeDays){
+    //     clearInterval();
+    //   }else{
+    //     finalDays++;
+    //   }
+    // }, 100);
 
     let timeHours = Math.floor((diff % days) / hours);
     timeHours = timeHours < 10 ? "0" + timeHours : timeHours;
 
-    let finalHours = 0;
-    setInterval(() => {
-      document.getElementById("hrs").innerHTML = finalHours;
+    // let finalHours = 0;
+    // setInterval(() => {
+    //   finalHours = finalHours < 10 ? "0" + finalHours : finalHours;
+    //   document.getElementById("hrs").innerHTML = finalHours;
 
-      if(finalHours === timeHours){
-        clearInterval();
-      }else{
-        finalHours++;
-      }
-    }, 100);
+    //   if(finalHours === timeHours){
+    //     clearInterval();
+    //   }else{
+    //     finalHours++;
+    //   }
+    // }, 100);
   
     let timeMinutes = Math.floor((diff % hours) / minutes);
     timeMinutes = timeMinutes < 10 ? "0" + timeMinutes : timeMinutes;
   
     let timeSeconds = Math.floor((diff % minutes) / seconds);
     timeSeconds = timeSeconds < 10 ? "0" + timeSeconds : timeSeconds;
-  
+
+    document.getElementById("days").innerHTML = timeDays;
+    document.getElementById("hrs").innerHTML = timeHours;
     document.getElementById("mins").innerHTML = timeMinutes;
     document.getElementById("secs").innerHTML = timeSeconds;
   }
